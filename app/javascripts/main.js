0f4daa6fd29205067f4f293c9468bcb1848ff7ba
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-12-11 18:41:50 -0800

    updated JSON for HTML parsing

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -1,15 +1,19 @@
 'use strict';
 
-var FeedApp = angular.module('FeedApp', []);
+var FeedApp = angular.module('FeedApp', ['ngSanitize']);
 
-function NewsCtrl($scope, $http) {
+function NewsCtrl($scope, $http, $sce) {
 
 	$scope.news = [];
 
-	$http.get('app/mock_json/nbc.json')
+	$http.get('app/mock_json/nbc-html.json')
 		.success(function(data){
 			console.log(data);
 			$scope.news.push(data);
 			
 		});
+
+	$scope.TrustSnippet = function(body) {
+		return $sce.trustAsHtml(body);
+	};	
 }
